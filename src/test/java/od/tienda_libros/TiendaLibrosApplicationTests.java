86585612f0f0112cdc3aa672c5bd92ee47f357eb
William Lopez R <oriondev2024@gmail.com>
2024-12-13 21:02:17 -0500

    proyecto base, configuración de aplicación para la creación de la base de datos y creación de proyecto base

---
+++ src/test/java/od/tienda_libros/TiendaLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package od.tienda_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
