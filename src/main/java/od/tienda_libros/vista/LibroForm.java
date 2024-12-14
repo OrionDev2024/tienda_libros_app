e9aa7c7e216322c959ba26eb6ba16b17e33b57bf
William Lopez R <oriondev2024@gmail.com>
2024-12-13 22:22:41 -0500

    creacion de tabla con sus cabeceros (swing)

---
+++ src/main/java/od/tienda_libros/vista/LibroForm.java
@@ -0,0 +1,45 @@
+package od.tienda_libros.vista;
+
+import od.tienda_libros.servicio.LibroServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+@Component
+public class LibroForm extends JFrame {
+
+    LibroServicio libroServicio;
+    private JPanel panel;
+    private JTable tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
+
+    @Autowired
+    public LibroForm(LibroServicio libroServicio){
+        this.libroServicio = libroServicio;
+        iniciarForma();
+    }
+
+    private void iniciarForma(){
+        setContentPane(panel );
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900, 700);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth() / 2);
+        int y = (tamanioPantalla.height - getHeight() / 2);
+        setLocation(x,y);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaModeloLibros = new DefaultTableModel(0, 5);
+        String[] cabeceros = {"Id", "Libro", "Autor", "Precio", "Existencias"};
+        tablaModeloLibros.setColumnIdentifiers(cabeceros);
+        //instanciamos el objeto JTable
+        this.tablaLibros = new JTable(tablaModeloLibros);
+    }
+}
